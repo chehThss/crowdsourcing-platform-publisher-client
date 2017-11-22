0f5065f7bde3ea777f94531f89711ab204bdc104
Che Hang <thss15_cheh@163.com>
2017-11-23 02:37:42 +0800

    create login dialog

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,27 @@
+<style lang="less">
+  .vertical-center-modal{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+<template>
+  <Button @click="modal10 = true">Vertical center</Button>
+  <Modal
+    title="Title"
+    v-model="modal10"
+    class-name="vertical-center-modal">
+    <p>Content of dialog</p>
+    <p>Content of dialog</p>
+    <p>Content of dialog</p>
+  </Modal>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        modal10: false,
+      }
+    }
+  }
+</script>
